69e4410efcdc3981a040db7df7e913e054ab7384
Dawit A <dndawita@gmail.com>
2023-06-25 12:01:56 -0400

    Create React App

---
+++ src/client.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const URL = 'https://uslnwaullsjhopukbhte.supabase.co';
+
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzbG53YXVsbHNqaG9wdWtiaHRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc3MDc5NTUsImV4cCI6MjAwMzI4Mzk1NX0._1CM5Qp6xsdYhbxIode5KzrIilBa9SWsa2dy-iB6WIg';
+
+export const supabase = createClient(URL, API_KEY);
